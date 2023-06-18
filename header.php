7b802fff1831cc5031222e23e713d19631f38d29
Filipe Ferreira <fferreira@cadastra.com>
2023-06-18 15:57:00 -0300

    Estrutura Inicial do Wordpress

---
+++ header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body <?php body_class(); ?> >
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Quas, excepturi minus facere error earum harum aliquam unde placeat totam. 
+        Doloremque inventore facere expedita consequatur at! Distinctio dicta magni doloribus nobis.
+    </p>
+    <div id="page" class="site">
+        <header>
+            <section class="top-bar">
+                <div class="logo">
+                    Logo
+                </div>
+                <div class="searchbox">
+                    search
+                </div>
+            </section>
+            <section class="menu-area">
+                <nav class="main-menu">
+                    menu
+                </nav>
+            </section>
+        </header>
