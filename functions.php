10d51f3e7124e64107cdb39f35c8d928bfbd6be3
Filipe Ferreira <fferreira@cadastra.com>
2023-06-18 21:35:16 -0300

    add css theme

--- functions.php
+++ functions.php
@@ -2,6 +2,7 @@
 
 function theme_load_scripts(){
     wp_enqueue_style('style', get_stylesheet_uri(), array(), wp_get_theme()->get('Version'), 'all');
+    wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap', array(), null);
 }
 
 add_action('wp_enqueue_scripts', 'theme_load_scripts');
