2bf0e83517e283402d19931aea829da38a163935
Filipe Ferreira <fferreira@cadastra.com>
2023-06-27 21:41:36 -0300

    add customize thumbnails

--- index.php
+++ index.php
@@ -12,6 +12,12 @@
                         ?>
                         <article>
                             <h2><?php the_title(); ?></h2>
+                            <?php 
+                                // Exibe a miniatura das imagens destacadas nos posts
+                                // Seleciona o tamanho do wordpress que é selecionado nas configurações > mídia
+                                // Ex: 'thumb', 'medium', 'large', 'full' ou personalizado com um array(275, 275)
+                                the_post_thumbnail(array(275, 275)); 
+                            ?>
                             <div class="meta-info">
                                 <p>Postado em <?php echo get_the_date(); ?> Por <?php the_author_posts_link(); ?></p>
                                 <p>Categoria: <?php the_category(' '); ?></p>
