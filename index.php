8008dcc2e195640b5c5a0e981fb9b3f69c1462da
Filipe Ferreira <fferreira@cadastra.com>
2023-07-07 12:17:58 -0300

    add permalink

--- index.php
+++ index.php
@@ -11,19 +11,26 @@
                                 while(have_posts()) : the_post();
                         ?>
                         <article>
-                            <h2><?php the_title(); ?></h2>
-                            <?php 
-                                // Exibe a miniatura das imagens destacadas nos posts
-                                // Seleciona o tamanho do wordpress que é selecionado nas configurações > mídia
-                                // Ex: 'thumb', 'medium', 'large', 'full' ou personalizado com um array(275, 275)
-                                the_post_thumbnail(array(275, 275)); 
-                            ?>
+                            <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                            <a href="<?php the_permalink(); ?>">
+                                <?php 
+                                    // Exibe a miniatura das imagens destacadas nos posts
+                                    // Seleciona o tamanho do wordpress que é selecionado nas configurações > mídia
+                                    // Ex: 'thumb', 'medium', 'large', 'full' ou personalizado com um array(275, 275)
+                                    the_post_thumbnail(array(275, 275)); 
+                                ?>
+                            </a>
                             <div class="meta-info">
                                 <p>Postado em <?php echo get_the_date(); ?> Por <?php the_author_posts_link(); ?></p>
                                 <p>Categoria: <?php the_category(' '); ?></p>
                                 <p>Tags: <?php the_tags('', ', '); ?></p>
                             </div>
-                            <?php the_content(); ?>
+                            <?php 
+                                // Content exibe conteúdo completo
+                                // excerpt exibe resumo
+                                //the_content();
+                                the_excerpt();
+                            ?>
                         </article>
                         <?php
                                 endwhile;
--- page-home.php
+++ page-home.php
@@ -48,13 +48,15 @@
                                 while($postlist->have_posts()) : $postlist->the_post();
                         ?>
                         <article class="latest-news">
-                            <?php 
-                                // Exibe a miniatura das imagens destacadas nos posts
-                                // Seleciona o tamanho do wordpress que é selecionado nas configurações > mídia
-                                // Ex: 'thumb', 'medium', 'large', 'full' ou personalizado com um array(275, 275)
-                                the_post_thumbnail('large'); 
-                            ?>
-                            <h3><?php the_title(); ?></h3>
+                            <a href="<?php the_permalink(); ?>">
+                                <?php 
+                                    // Exibe a miniatura das imagens destacadas nos posts
+                                    // Seleciona o tamanho do wordpress que é selecionado nas configurações > mídia
+                                    // Ex: 'thumb', 'medium', 'large', 'full' ou personalizado com um array(275, 275)
+                                    the_post_thumbnail('large'); 
+                                ?>
+                            </a>
+                            <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
                             <div class="meta-info">
                                 <p>
                                     Por <span><?php the_author_posts_link(); ?></span>
